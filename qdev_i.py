f78d313012ad00649fce05c7b7dcb55b8429ee91
Zack P <zack@zacklearns.com>
2017-07-21 21:59:42 -0600

    Add files via upload

---
+++ qdev_i.py
@@ -0,0 +1,33 @@
+﻿from pyparsing import *
+import os
+os.system('getCameras.py "path_to_the_scene" ')
+data = open(os.system('qdev.py "path_to_the_scene" '),"r")
+
+LBRACE,RBRACE,LPAREN,RPAREN,SEMI = map(Suppress,"{}();")
+GROUP = Keyword("group")
+ENTRY = Keyword("enter")
+
+real = Regex(r"[+-]?\d+\.\d*").setParseAction(lambda t:float(t[0]))
+integer = Regex(r"[+-]?\d+").setParseAction(lambda t:int(t[0]))
+
+# parses a string enclosed in quotes, but strips off the quotes at parse time
+string = QuotedString('"')
+
+# define structure expressions
+value = string | real | integer
+entry = Group(ENTRY + LPAREN + Group(Optional(delimitedList(value)))) + RPAREN + SEMI
+
+# since Groups can contain Groups, need to use a Forward to define recursive expression
+group = Forward()
+group << Group(GROUP + LPAREN + string("name") + RPAREN + 
+            LBRACE + Group(ZeroOrMore(group | entry))("body") + RBRACE)
+
+# ignore C style comments wherever they occur
+group.ignore(cStyleComment)
+
+# parse the sample text
+result = group.parseString(data)
+
+# print out the tokens as a nice indented list using pprint
+from pprint import pprint
+pprint(result.asList())
